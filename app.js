ce2016423a002a26d91fdad31a478e6c00c88333
Mazzuca <fmazzuca@minsait.com>
2021-10-27 10:00:16 +0200

    prova

--- app.js
+++ app.js
@@ -18,12 +18,10 @@ app.use(bodyParser.urlencoded({
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
-
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-
 
 userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"] });
 
